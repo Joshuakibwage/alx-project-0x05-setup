c9e054ee7f47c73129e773ab902c3d9da5c2efe6
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-08-09 17:55:57 +0300

    feat: add content in index component

--- alx-project-0x07/pages/_app.tsx
+++ alx-project-0x07/pages/_app.tsx
@@ -1,6 +1,8 @@
+import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "@/components/layouts/Layout";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
