3e6e3239999d691576284f6812837a89da22ca60
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-08-09 18:17:21 +0300

    chore: copy alx-project-0x07 to alx-project-0x08

---
+++ alx-project-0x08/components/layouts/Layout.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/layouts/Header";
+import Footer from "@/components/layouts/Footer";
+
+
+
+
+const Layout: React.FC = ({ children }) => {
+    return (
+        <>
+            <Header />
+                <div className="min-h-screen">
+                    {children}
+                </div>
+            <Footer />
+        </>
+    )
+}
+
+
+export default Layout;
